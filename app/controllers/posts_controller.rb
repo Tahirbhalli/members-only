1861eae00244bbe5dab2bd941407f5ffab427dc5
SpaYco <zizou.aziz101@gmail.com>
2020-04-27 15:44:30 +0100

    fixed rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,27 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!
-    def index
-      @list = Post.all
+  before_action :authenticate_user!
+  def index
+    @list = Post.all
+  end
+
+  def new
+    @post = Post.new
+    @user_id = current_user.id
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
     end
-  
-    def new
-      @post = Post.new
-      @user_id = current_user.id
-    end
-  
-    def create
-      @post = Post.new(post_params)
-  
-      if @post.save
-        redirect_to posts_path
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def post_params
-      params.require(:post).permit(:content, :title, :user_id)
-    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :title, :user_id)
+  end
 end
