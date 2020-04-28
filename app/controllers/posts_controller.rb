62c3a4589c8e618d5c429d93e688528dacfc0679
SpaYco <zizou.aziz101@gmail.com>
2020-04-28 08:31:04 +0100

    added loging and signout links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :new]
   def index
     @list = User.joins(:posts).select('posts.title, Posts.content,users.email')
   end
