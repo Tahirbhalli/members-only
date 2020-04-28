b414816115415e089779b1d3481979ab0e109ffa
SpaYco <zizou.aziz101@gmail.com>
2020-04-28 08:14:00 +0100

    fixed security with user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to posts_path
@@ -22,6 +23,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content, :title, :user_id)
+    params.require(:post).permit(:content, :title)
   end
 end
