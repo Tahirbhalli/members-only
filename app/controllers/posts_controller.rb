4801b0ffd76cafbdb4cc168595e2f532922e7352
SpaYco <zizou.aziz101@gmail.com>
2020-04-28 08:32:29 +0100

    Fixed Rubocop Issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new]
+  before_action :authenticate_user!, only: %i[create new]
   def index
     @list = User.joins(:posts).select('posts.title, Posts.content,users.email')
   end
